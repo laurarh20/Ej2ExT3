a121e6993a049099bd71639b361b76c58ed4a998
laurarh20 <lrubio5278@ceuandalucia.es>
2023-12-01 19:59:12 +0100

    soy Laura Rubio y me encantan los polvorones

---
+++ ProyectoExEj2/src/Polvoron.java
@@ -0,0 +1,10 @@
+
+public class Polvoron {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Soy Laura rubio Hurtado y me encantan los polvorones");
+	}
+
+}
