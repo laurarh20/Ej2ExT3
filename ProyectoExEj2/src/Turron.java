60c64d57bc8a5aeca2032d68b5803fe54ed1c597
laurarh20 <lrubio5278@ceuandalucia.es>
2023-12-01 20:02:47 +0100

    Subo turron a local

---
+++ ProyectoExEj2/src/Turron.java
@@ -0,0 +1,10 @@
+
+public class Turron {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("soy Laura rubio y me encantan los turrones");
+	}
+
+}
