5db9e8ebb5355215f08d6cb1710a146ffca1aebc
laurarh20 <lrubio5278@ceuandalucia.es>
2023-12-01 20:06:31 +0100

    Hago un push de turron

--- ProyectoExEj2/src/Turron.java
+++ ProyectoExEj2/src/Turron.java
@@ -5,6 +5,7 @@ public class Turron {
 		// TODO Auto-generated method stub
 
 		System.out.println("soy Laura rubio y me encantan los turrones");
+		System.out.println("pero mi favorito es el de chocolate");
 	}
 
 }
